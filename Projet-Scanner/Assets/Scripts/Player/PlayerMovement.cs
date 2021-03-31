7530fdacdcc5cdc58c39be60cc89354e14786a95
unknown <73117777+Romain945@users.noreply.github.com>
2021-03-31 15:31:34 +0200

    Scene fps

---
+++ Projet-Scanner/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Tooltip("Player move speed")]
+    [SerializeField] float m_MoveSpeed;
+
+    [Tooltip("Player spawn")]
+    [SerializeField] Transform m_SpawnPoint;
+
+    Transform m_Transform;
+    Rigidbody m_Rigidbody;
+
+    void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = (m_Transform.right * hAxis + m_Transform.forward * vAxis);
+        m_Rigidbody.AddForce(movement * m_MoveSpeed, ForceMode.VelocityChange);
+    }
+
+    void Reset()
+    {
+        m_Rigidbody.position = m_SpawnPoint.position;
+        m_Rigidbody.velocity = Vector3.zero;
+        m_Rigidbody.angularVelocity = Vector3.zero;
+    }
+}
---
+++ Projet-Scanner/Assets/Scripts/Player/PlayerRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [Tooltip("Player Transform for horizontal rotation")]
+    [SerializeField] Transform m_PlayerTransform;
+
+    Transform m_Transform;
+
+    float xRotation = 0f;
+    [SerializeField] float m_MouseSensitivity;
+
+    void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+
+        float mouseY = Input.GetAxis("Mouse Y") * m_MouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * m_MouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        m_Transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Rotation de la tête horizontale et verticale
+        m_PlayerTransform.Rotate(Vector3.up * mouseX); // Rotation du joueur a l'horizontale seulement (pour les déplacements)
+    }
+
+    void Reset()
+    {
+        xRotation = 0f;
+        m_Transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Rotation de la tête horizontale et verticale
+        m_PlayerTransform.rotation = Quaternion.identity; // Rotation du joueur a l'horizontale seulement (pour les déplacements)
+    }
+}
