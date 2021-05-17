476fed76b68f3562736aa049ebd64cad68b27d25
Romain Emery <romain.emery@edu.itescia.fr>
2021-05-17 23:29:06 +0200

    Ambiance et jeu de lumière

--- Projet-Scanner/Assets/Scripts/Managers/Common/Level.cs
+++ Projet-Scanner/Assets/Scripts/Managers/Common/Level.cs
@@ -18,6 +18,9 @@ public class Level : MonoBehaviour, IEventHandler
 	[SerializeField] Light[] m_LevelLight;
 	float m_InitialIntensity;
 
+	Light m_PlayerLight;
+	float m_InitialPlayerIntensity;
+
 	float m_Timer = 0;
 
 	#region Events' subscription
@@ -45,6 +48,8 @@ public class Level : MonoBehaviour, IEventHandler
     {
 		m_ObjectInLevel = FindObjectsOfType<PickupObject>().ToList();
 		m_InitialIntensity = m_LevelLight[0].intensity;
+		m_PlayerLight = GameObject.FindGameObjectWithTag("PlayerLight").GetComponent<Light>();
+		m_InitialPlayerIntensity = m_PlayerLight.intensity;
 	}
 
 	void ObjectHasBeenDestroy(ObjectHasBeenDestroyEvent e)
@@ -80,34 +85,121 @@ public class Level : MonoBehaviour, IEventHandler
 	IEnumerator BlinkingLight()
     {
 		int n = 0;
+        #region Première phase
         while (GameManager.Instance.IsPlaying)
         {
 			for (int i = 0; i < m_LevelLight.Length; i++)
-				m_LevelLight[i].intensity = 0;
+				m_LevelLight[i].transform.parent.gameObject.SetActive(false);
 			yield return new WaitForSeconds(Random.Range(.035f, .065f));
 			for (int i = 0; i < m_LevelLight.Length; i++)
-				m_LevelLight[i].intensity = m_InitialIntensity;
+				m_LevelLight[i].transform.parent.gameObject.SetActive(true);
 			yield return new WaitForSeconds(Random.Range(.05f, 3));
 			n++;
-			if(n > 20)
+			if(n > 5)
 				break;
 		}
+        #endregion
 
-		for (int i = 0; i < m_LevelLight.Length; i++)
-			m_LevelLight[i].intensity = 0;
-		yield return new WaitForSeconds(Random.Range(3,6));
+        #region Coupure et baisse d'intensité
+        for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].transform.parent.gameObject.SetActive(false);
+
+		m_InitialIntensity = 0.5f;
+
 		for (int i = 0; i < m_LevelLight.Length; i++)
 			m_LevelLight[i].intensity = m_InitialIntensity;
-		yield return new WaitForSeconds(Random.Range(.05f, 1));
+		yield return new WaitForSeconds(Random.Range(4,7));
 
-		while (GameManager.Instance.IsPlaying)
+		for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].transform.parent.gameObject.SetActive(true);
+		yield return new WaitForSeconds(Random.Range(.05f, 1));
+		n = 0;
+        #endregion
+
+        #region Deuxième phase
+        while (GameManager.Instance.IsPlaying)
 		{
 			for (int i = 0; i < m_LevelLight.Length; i++)
-				m_LevelLight[i].intensity = 0;
+				m_LevelLight[i].transform.parent.gameObject.SetActive(false);
 			yield return new WaitForSeconds(Random.Range(.035f, .065f));
 			for (int i = 0; i < m_LevelLight.Length; i++)
-				m_LevelLight[i].intensity = m_InitialIntensity;
-			yield return new WaitForSeconds(Random.Range(.05f, 3));
+				m_LevelLight[i].transform.parent.gameObject.SetActive(true);
+			yield return new WaitForSeconds(Random.Range(.05f, 2.5f));
+			n++;
+			if (n > 30)
+				break;
 		}
+        #endregion
+
+        #region Deuxième coupure et arrêt lumière
+        for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].transform.parent.gameObject.SetActive(false);
+
+		m_InitialIntensity = 0.15f;
+
+		for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].intensity = m_InitialIntensity;
+		yield return new WaitForSeconds(Random.Range(4, 7));
+
+		for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].transform.parent.gameObject.SetActive(true);
+		yield return new WaitForSeconds(Random.Range(1, 2));
+		for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].transform.parent.gameObject.SetActive(false);
+		yield return new WaitForSeconds(Random.Range(.035f, .065f));
+		for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].transform.parent.gameObject.SetActive(true);
+		yield return new WaitForSeconds(Random.Range(.05f, .095f));
+		for (int i = 0; i < m_LevelLight.Length; i++)
+			m_LevelLight[i].transform.parent.gameObject.SetActive(false);
+
+		yield return new WaitForSeconds(Random.Range(10, 20));
+		n = 0;
+		#endregion
+
+		#region Première Lampe bug
+		while (GameManager.Instance.IsPlaying)
+		{
+			m_PlayerLight.intensity = Random.Range(0.05f, 0.3f);
+			yield return new WaitForSeconds(Random.Range(.035f, .065f));
+			m_PlayerLight.intensity = m_InitialPlayerIntensity;
+			yield return new WaitForSeconds(Random.Range(.05f, 3));
+			n++;
+			if (n > 15)
+				break;
+		}
+        #endregion
+
+        #region Coupe Lampe
+        m_PlayerLight.intensity = 0;
+		m_InitialPlayerIntensity = 0.2f;
+		yield return new WaitForSeconds(Random.Range(0.8f, 1.5f));
+
+		m_PlayerLight.intensity = m_InitialPlayerIntensity;
+		yield return new WaitForSeconds(Random.Range(.035f, .065f));
+		m_PlayerLight.intensity = Random.Range(0.025f, 0.15f);
+		yield return new WaitForSeconds(Random.Range(.035f, .065f));
+		m_PlayerLight.intensity = m_InitialPlayerIntensity;
+		yield return new WaitForSeconds(Random.Range(.035f, .065f));
+		m_PlayerLight.intensity = Random.Range(0.025f, 0.15f);
+		yield return new WaitForSeconds(Random.Range(.035f, .065f));
+		m_PlayerLight.intensity = m_InitialPlayerIntensity;
+		m_InitialPlayerIntensity = 0.3f;
+		yield return new WaitForSeconds(Random.Range(4, 6));
+		n = 0;
+        #endregion
+
+        #region Deuxième Lampe bug
+        while (GameManager.Instance.IsPlaying)
+		{
+			m_PlayerLight.intensity = Random.Range(0.025f, 0.2f);
+			yield return new WaitForSeconds(Random.Range(.035f, .065f));
+			m_PlayerLight.intensity = m_InitialPlayerIntensity;
+			yield return new WaitForSeconds(Random.Range(.05f, 3));
+			n++;
+			if (n > 10)
+				break;
+		}
+		#endregion
 	}
 }
